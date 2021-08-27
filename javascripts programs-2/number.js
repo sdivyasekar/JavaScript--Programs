0284cd45ecae52ab0efe5d2d8e416145602e36ec
sdivyasekar <divyask127@gmail.com>
2021-08-27 18:26:25 +0530

    JavaScript Programs
    Added Basic basic concepts of JavaScript Programs-Day2 Learning

---
+++ javascripts programs-2/number.js
@@ -0,0 +1,22 @@
+var x="4"-2;
+console.log(x);
+console.log(typeof(x));
+const a = 0xff;
+const b=0xee;
+console.log(a);
+console.log(b);
+const c=5e5-5;
+console.log(c);
+console.log("hello"+2);
+console.log(2+"hello");
+console.log(12345678998765434366789654+1); 
+console.log(12345678998765433444789952n+1n);
+const d = 99999999999999999; //took the round off value for the 15 didit number only the integer starts with same number//It is not a safest Integer.
+console.log(d);
+console.log(parseInt("20.05"));
+console.log(parseInt(20.05));
+var g="Hello";
+var x=new String("4");
+console.log(g==x);
+var h=new String("Hello");
+console.log(g==h);
---
+++ javascripts programs-2/string.js
@@ -0,0 +1,39 @@
+var x=2;
+var y=3;
+var sum=x+y;
+var xcv=`the sum of the two numbers are ${sum} hwegwuglsghw`;                              
+console.log(xcv);
+console.log(x==y)
+var q="hr";
+var w="Hr";
+console.log(x==y);
+console.log(xcv.charAt(6));
+console.log(xcv[9]);
+var sub=`the add message ${sum}\ 
+the output result`;
+console.log(sub);
+var b=new String("Hello");
+console.log(typeof(b));
+console.log(xcv.length); 
+const num1="Hello";
+const num2="Everyone";
+const re1=num1.charAt(3);//finding the indez place
+console.log(re1);
+const re2=num1.concat(" ",num2); //cocadenation
+console.log(re2);
+const num3="byebye thanks";
+const re3=num3.replace(num3,"come");//replace the string
+console.log(re3);
+console.log(num2.split()); //converts the string to array
+console.log(num2.substr(1,4)); //print all the character from 1-4;
+console.log(num2.substring(1,4));
+console.log(num2.slice(1,4));
+console.log(num2.toUpperCase());
+console.log(num2.toLowerCase());
+console.log(num3.trim());//cut the excess space
+console.log(num3.includes(num3,"he","po"));
+const vir="Everyone"-"4";
+console.log(vir);
+console.log("4"-"2");
+console.log("4"-2);
+console.log(String(x));
